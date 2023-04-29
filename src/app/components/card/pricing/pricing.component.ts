a7008f6049fc0fa8cba84aa498a448e647c092a3
Rodrigo <rodrigoml2395@gmail.com>
2023-04-29 08:34:22 -0300

    feat: pricing

--- src/app/components/card/pricing/pricing.component.ts
+++ src/app/components/card/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./pricing.component.css']
 })
 export class PricingComponent {
-
+  @Input()
+  gameType: string = "Digital PS4"
+  @Input()
+  gamePrice: string = "R$ 399,90"
 }
